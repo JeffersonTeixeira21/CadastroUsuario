a128edeef81bf47ffd4048a7b1946e378f7948e4
JEFFERSON TEIXEIRA <jefferson_teixeira1@estudante.sesisenai.org.br>
2025-03-20 20:11:58 -0300

    Adicionando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Register from "./pages/Register"
 import Profile from "./pages/Profile"
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <Router>
+      <Header /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </Router>
+    </Router>   
 
   )
 }
