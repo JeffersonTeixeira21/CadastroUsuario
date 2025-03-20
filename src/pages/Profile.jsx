9451151847969b2fd3dd84742e284732577ed28e
JEFFERSON TEIXEIRA <jefferson_teixeira1@estudante.sesisenai.org.br>
2025-03-20 19:29:31 -0300

    Criando pages

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+function Profile() {
+    return (
+        <div>
+        </div>
+    );
+}
+export default Profile;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+function Register() {
+    return (
+        <div>
+        </div>
+    );
+}
+export default Register;
